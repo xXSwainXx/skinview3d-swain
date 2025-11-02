de950cc36a34c57e56ebdd0d85feac67cbe40fed
David Katalinic <76821023+xXSwainXx@users.noreply.github.com>
2025-11-02 23:11:17 +0100

    Refactor paint method for improved clarity

--- libs/nametag.js
+++ libs/nametag.js
@@ -39,111 +39,119 @@ export class NameTagObject extends Sprite {
     }
 
     async paint() {
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext("2d");
-        
-        const rankFont = this.isBold ? `bold ${this.font}` : this.font;
-        const textFont = this.font;
-        
-        ctx.font = rankFont;
-        
-        const beforeMetrics = ctx.measureText(this.isPremiumRank ? this.text.split(this.divider)[0] : this.text);
-        const afterMetrics = this.isPremiumRank ? ctx.measureText(this.text.split(this.divider)[1]) : undefined;
-        const dividerMetrics = this.isPremiumRank ? ctx.measureText(this.divider) : undefined;
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
 
-        let imageWidth = 0;
-        let imageHeight = 0;
-        let imageLoaded = false;
-        let loadedImage = null;
+    const rankFont = this.isBold ? `bold ${this.font}` : this.font;
+    const textFont = this.font;
 
-        if (this.rankImage && this.isPremiumRank) {
-            try {
-                const img = await this.loadImage(this.rankImage);
-                imageWidth = (img.width / img.height) * this.imageHeight;
-                imageHeight = this.imageHeight;
-                imageLoaded = true;
-                loadedImage = img;
-            } catch (error) {
-                console.error("Failed to load rank image:", error);
-                imageLoaded = false;
-            }
+    ctx.font = rankFont;
+
+    const beforeMetrics = ctx.measureText(this.isPremiumRank ? this.text.split(this.divider)[0] : this.text);
+    const afterMetrics = this.isPremiumRank ? ctx.measureText(this.text.split(this.divider)[1]) : undefined;
+    const dividerMetrics = this.isPremiumRank ? ctx.measureText(this.divider) : undefined;
+
+    let imageWidth = 0;
+    let imageHeight = 0;
+    let imageLoaded = false;
+    let loadedImage = null;
+
+    if (this.rankImage && this.isPremiumRank) {
+        try {
+            const img = await this.loadImage(this.rankImage);
+            imageWidth = (img.width / img.height) * this.imageHeight;
+            imageHeight = this.imageHeight;
+            imageLoaded = true;
+            loadedImage = img;
+        } catch (error) {
+            console.error("Failed to load rank image:", error);
+            imageLoaded = false;
         }
+    }
 
-        const imageSectionWidth = imageLoaded ? 
-            this.margin[3] + imageWidth + this.margin[1] : 
-            this.margin[3] + beforeMetrics.width + this.margin[1];
+    const imageSectionWidth = imageLoaded
+        ? this.margin[3] + imageWidth + this.margin[1]
+        : this.margin[3] + beforeMetrics.width + this.margin[1];
 
-        canvas.width = imageSectionWidth +
-            (this.isPremiumRank ? dividerMetrics.width + this.margin[1] : 0) +
-            (this.isPremiumRank ? afterMetrics.width : 0) +
-            this.margin[1];
+    const dpr = window.devicePixelRatio || 1;
 
-        canvas.height = this.margin[0] + 
-            Math.max(
-                beforeMetrics.actualBoundingBoxAscent + beforeMetrics.actualBoundingBoxDescent,
-                imageHeight
-            ) + 
-            this.margin[2];
+    const logicalWidth =
+        imageSectionWidth +
+        (this.isPremiumRank ? dividerMetrics.width + this.margin[1] : 0) +
+        (this.isPremiumRank ? afterMetrics.width : 0) +
+        this.margin[1];
 
-        ctx.fillStyle = this.backgroundStyle;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    const logicalHeight =
+        this.margin[0] +
+        Math.max(
+            beforeMetrics.actualBoundingBoxAscent + beforeMetrics.actualBoundingBoxDescent,
+            imageHeight
+        ) +
+        this.margin[2];
 
-        if (this.isPremiumRank) {
-            if (imageLoaded && this.rankImage) {
-                const img = loadedImage;
-                const yPos = this.margin[0] + (canvas.height - this.margin[0] - this.margin[2] - imageHeight) / 2;
-                
-                // NUR DIESEN TEIL FÜR BESSERE BILDQUALITÄT GEÄNDERT:
-                ctx.imageSmoothingEnabled = true;
-                ctx.imageSmoothingQuality = 'high';
-                ctx.drawImage(img, this.margin[3], yPos, imageWidth, imageHeight);
-                
-            } else {
-                ctx.font = rankFont;
-                ctx.fillStyle = this.textStyle;
-                ctx.fillText(
-                    this.text.split(this.divider)[0], 
-                    this.margin[3], 
-                    this.margin[0] + beforeMetrics.actualBoundingBoxAscent
-                );
-            }
+    canvas.width = logicalWidth * dpr;
+    canvas.height = logicalHeight * dpr;
 
-            const dividerXPos = imageLoaded ? 
-                this.margin[3] + imageWidth + this.margin[1] : 
-                this.margin[3] + beforeMetrics.width + this.margin[1];
+    ctx.scale(dpr, dpr);
 
-            ctx.fillStyle = this.dividerStyle;
-            ctx.fillText(
-                this.divider, 
-                dividerXPos, 
-                this.margin[0] + dividerMetrics.actualBoundingBoxAscent
-            );
+    ctx.imageSmoothingEnabled = true;
+    ctx.imageSmoothingQuality = "high";
 
-            ctx.font = textFont;
-            ctx.fillStyle = this.textStyle;
-            ctx.fillText(
-                this.text.split(this.divider)[1], 
-                dividerXPos + dividerMetrics.width + this.margin[1], 
-                this.margin[0] + afterMetrics.actualBoundingBoxAscent
-            );
+    ctx.fillStyle = this.backgroundStyle;
+    ctx.fillRect(0, 0, logicalWidth, logicalHeight);
+
+    if (this.isPremiumRank) {
+        if (imageLoaded && this.rankImage) {
+            const img = loadedImage;
+            const yPos = this.margin[0] + (logicalHeight - this.margin[0] - this.margin[2] - imageHeight) / 2;
+            ctx.drawImage(img, this.margin[3], yPos, imageWidth, imageHeight);
         } else {
-            ctx.font = textFont;
+            ctx.font = rankFont;
             ctx.fillStyle = this.textStyle;
             ctx.fillText(
-                this.text, 
-                this.margin[3], 
+                this.text.split(this.divider)[0],
+                this.margin[3],
                 this.margin[0] + beforeMetrics.actualBoundingBoxAscent
             );
         }
 
-        const texture = new CanvasTexture(canvas);
-        texture.magFilter = NearestFilter;
-        texture.minFilter = NearestFilter;
-        this.textMaterial.map = texture;
-        this.textMaterial.needsUpdate = true;
-        this.scale.x = (canvas.width / canvas.height) * this.height;
-        this.scale.y = this.height;
+        const dividerXPos = imageLoaded
+            ? this.margin[3] + imageWidth + this.margin[1]
+            : this.margin[3] + beforeMetrics.width + this.margin[1];
+
+        ctx.fillStyle = this.dividerStyle;
+        ctx.fillText(
+            this.divider,
+            dividerXPos,
+            this.margin[0] + dividerMetrics.actualBoundingBoxAscent
+        );
+
+        ctx.font = textFont;
+        ctx.fillStyle = this.textStyle;
+        ctx.fillText(
+            this.text.split(this.divider)[1],
+            dividerXPos + dividerMetrics.width + this.margin[1],
+            this.margin[0] + afterMetrics.actualBoundingBoxAscent
+        );
+    } else {
+        ctx.font = textFont;
+        ctx.fillStyle = this.textStyle;
+        ctx.fillText(
+            this.text,
+            this.margin[3],
+            this.margin[0] + beforeMetrics.actualBoundingBoxAscent
+        );
     }
+
+    const texture = new CanvasTexture(canvas);
+    texture.magFilter = NearestFilter;
+    texture.minFilter = NearestFilter;
+    this.textMaterial.map = texture;
+    this.textMaterial.needsUpdate = true;
+
+    this.scale.x = (logicalWidth / logicalHeight) * this.height;
+    this.scale.y = this.height;
+}
 
     loadImage(src) {
         return new Promise((resolve, reject) => {
